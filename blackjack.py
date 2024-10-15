7cd50103d92817d6779aaaec563de0c6a38f11e9
bradyDarren <dcgbfinancial@gmail.com>
2024-10-15 11:07:55 -0500

    added for loop to make sure that when the hand is dealt the score is checked after every single card dealt

--- blackjack.py
+++ blackjack.py
@@ -34,36 +34,39 @@ def rank_eval(score):
         score = card[0]
     return score
 
-
-
 def blackjack():
     user_score = 0
     comp_score = 0 
     user_start = input("Would you like to play a game of blackjack? Type 'y' or 'n': ")
-    user_cards = [rank_eval(user_score), rank_eval(user_score)]
-    # need to make sure that the user score value is checked & updated after ever card is pulled. Not after the first two cards are pulled.
-    user_score += sum(user_cards)
-    print(user_cards)
-    comp_cards = [rank_eval(comp_score), rank_eval(comp_score)]
-    print(comp_cards)
-    comp_score += sum(comp_cards)
-    print(comp_score)
-    print(f"Your cards: {user_cards}, current score: {sum(user_cards)}")
-    print(f"Computer's first card: {comp_cards[0]}")
-    while user_start != 'n':
-        user_start = input("Type 'y' to get another card, type 'n' to pass: ")
-        if user_start == "n":
-            comp_cards.append(rank_eval(comp_cards))
-            print(f"Your final hand: {user_cards}, current score: {sum(user_cards)}")
-            print(f"Computer's final hand: {comp_cards}, final score {sum(comp_cards)}")
-        else: 
+    if user_start == "y":
+        user_cards = []
+        comp_cards = []
+        for card in range(2):
             user_cards.append(rank_eval(user_score))
-            print(f"Your cards: {user_cards}, current score: {sum(user_cards)}")
-        print(comp_score)
-        print(user_score)       
-        
-    # if user_score > 21:
-    #     print(f"Busted. ")
+            # print(user_cards) - test line
+            user_score = sum(user_cards)
+            # print(user_score) - test line
+            comp_cards.append(rank_eval(comp_score))
+            # print(comp_cards) - test line
+            comp_score = sum(comp_cards)
+            # print(comp_score) - test line
+        print(f"Your cards: {user_cards}, current score: {sum(user_cards)}")
+        print(f"Computer's first card: {comp_cards[0]}")
+        while user_start != 'n':
+            user_start = input("Type 'y' to get another card, type 'n' to pass: ")
+            if user_start == "n":
+                while sum(comp_cards) <= sum(user_cards):
+                    comp_cards.append(rank_eval(comp_score))
+                print(f"Your final hand: {user_cards}, current score: {sum(user_cards)}")
+                print(f"Computer's final hand: {comp_cards}, final score {sum(comp_cards)}")
+            else: 
+                user_cards.append(rank_eval(user_score))
+                print(f"Your cards: {user_cards}, current score: {sum(user_cards)}")
+            print(comp_score)
+            print(user_score)   
+    else: 
+        print("Thanks for coming. See you later!!!!")    
+            
 blackjack()
 
 
