6a2a0ebdf038dba1c25b3d0d7ecb64707d17b72f
bradyDarren <dcgbfinancial@gmail.com>
2024-10-16 05:50:13 -0500

    Working out the results of the hand played.

--- blackjack.py
+++ blackjack.py
@@ -37,7 +37,7 @@ def rank_eval(score):
 def blackjack():
     user_score = 0
     comp_score = 0 
-    user_start = input("Would you like to play a game of blackjack? Type 'y' or 'n': ")
+    user_start = input("Would you like to play a game of blackjack? Type 'y' or 'n': ").lower()
     if user_start == "y":
         user_cards = []
         comp_cards = []
@@ -51,19 +51,29 @@ def blackjack():
             comp_score = sum(comp_cards)
             # print(comp_score) - test line
         print(f"Your cards: {user_cards}, current score: {sum(user_cards)}")
-        print(f"Computer's first card: {comp_cards[0]}")
+        print(f"Computer's hand: {comp_cards[0]}")
         while user_start != 'n':
-            user_start = input("Type 'y' to get another card, type 'n' to pass: ")
+            user_start = input("Type 'y' to get another card, type 'n' to pass: ").lower()
             if user_start == "n":
                 while sum(comp_cards) <= sum(user_cards):
-                    comp_cards.append(rank_eval(comp_score))
+                    comp_cards.append(rank_eval(comp_score))                        
                 print(f"Your final hand: {user_cards}, current score: {sum(user_cards)}")
                 print(f"Computer's final hand: {comp_cards}, final score {sum(comp_cards)}")
+                if sum(comp_cards) > 21:
+                    print("House Busted. Challenger Wins!!!")        
             else: 
                 user_cards.append(rank_eval(user_score))
                 print(f"Your cards: {user_cards}, current score: {sum(user_cards)}")
-            print(comp_score)
-            print(user_score)   
+                print(f"Computer's hand: {comp_cards[0]}")
+                if sum(user_cards) > 21:
+                    print("Busted. House Wins!!!")
+
+        if sum(user_cards) > sum(comp_cards):
+            print("Challenger wins!!")
+        
+        if sum(comp_cards) > sum(user_cards):
+            print("House wins!!!")
+
     else: 
         print("Thanks for coming. See you later!!!!")    
             
